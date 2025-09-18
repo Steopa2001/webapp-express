81b86a7fd609a80a8a6ae4c30920ecbc5dc7d511
Steopa2001 <moraru495@gmail.com>
2025-09-18 16:41:14 +0200

    feat: enhance show method to include movie reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,42 +1,47 @@
-//importo la connessione al db
+// controllers/movies.js
+// importo la connessione al db
 const connection = require("../data/db");
 
-//index
+// GET /movies
 const index = (req, res) => {
-  // creo la query
   const sql = "SELECT * FROM movies";
-
-  // eseguo la query
   connection.query(sql, (err, results) => {
-    if (err)
-      return res
-        .status(500)
-        .json({ error: `Errore nell'esecuzione della query: ${err}` });
-
-    res.send(results);
+    if (err) return res.status(500).json({ error: `Errore query: ${err}` });
+    res.json(results);
   });
 };
 
-//show
+// GET /movies/:id (dettagli + recensioni)
 const show = (req, res) => {
-  //recupero l'id parametro
-  const { id } = req.params;
+  const id = Number(req.params.id);
+  if (!Number.isInteger(id) || id <= 0) {
+    return res.status(400).json({ error: "ID non valido" });
+  }
 
-  //creo la query
   const sqlMovie = "SELECT * FROM movies WHERE id = ?";
-
-  //eseguo la query passando ora i parametri
   connection.query(sqlMovie, [id], (err, resultMovie) => {
-    if (err)
-      return res
-        .status(500)
-        .json({ error: `errore nell'esecuzione della query: ${err}` });
+    if (err) return res.status(500).json({ error: `Errore query: ${err}` });
+    if (resultMovie.length === 0) {
+      return res.status(404).json({ error: "Film non trovato" });
+    }
 
-    res.send(resultMovie[0]);
+    const movie = resultMovie[0];
+
+    // recupero le recensioni collegate
+    const sqlReviews = `
+      SELECT r.*
+      FROM reviews r
+      WHERE r.movie_id = ?
+      ORDER BY r.created_at DESC
+    `;
+
+    connection.query(sqlReviews, [id], (err2, reviews) => {
+      if (err2) return res.status(500).json({ error: `Errore query: ${err2}` });
+
+      // rispondo con film + recensioni
+      res.json({ ...movie, reviews });
+    });
   });
 };
 
-module.exports = {
-  index,
-  show,
-};
+module.exports = { index, show };
