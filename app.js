6874f12797dcdb218de9bc8d623ba53d3588b041
Steopa2001 <moraru495@gmail.com>
2025-09-18 15:46:41 +0200

    feat: create a .env file for the security

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //importo express
 const express = require('express');
+const connection = require('./data/db');
 
 //creo l'istanza dell'app attraverso il metodo express che ho importato
 const app = express();
 
 // definisco il numero di porta su cui deve girare l'applicazione
-const port = 3000;
+const port = process.env.PORT;
 
 // definisco la rotta base  
 app.get('/', (req, res) => {
--- data/db.js
+++ data/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 //creo la connessione 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'SteopaRoot',
-    database: 'movies_db',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 //stabilisco la connessione al db
