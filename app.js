e285470f8a41b9d6a68ee5c337fd76b70d06a8f7
Steopa2001 <moraru495@gmail.com>
2025-09-21 17:53:25 +0200

    feat: add courses for multi-origin request

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 //creo l'istanza dell'app attraverso il metodo express che ho importato
 const app = express();
 
+//imlorto il pacchetto cors
+const cors = require('cors');
+
 // definisco il numero di porta su cui deve girare l'applicazione
 const port = process.env.PORT;
 
@@ -11,6 +14,9 @@ const port = process.env.PORT;
 const movieRouter = require('./routers/movieRouter');
 const notFound = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
+
+//registro il middleware per il cors
+app.use(cors({origin: process.end.FE_APP}))
 
 app.use(express.static('public'));  
 
